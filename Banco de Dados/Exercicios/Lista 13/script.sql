d05e1ab80a26411b78bcd708c0a0b69790f9dd30
DanyloSPTech <danylo.gomes@sptech.school>
2022-05-17 23:05:26 -0300

    continuacao exercicios banco - exer 16

--- Banco de Dados/Exercicios/Lista 13/script.sql
+++ Banco de Dados/Exercicios/Lista 13/script.sql
@@ -90,7 +90,65 @@ INSERT INTO FuncProj (fkFunc, fkProj, horas)
             (4, 20, 15.0),
             (8, 20, NULL);
             
-select * from Departamento;
-select * from Funcionario;
-select * from Projeto;
-select * from FuncProj;
+-- Configurando a chave estrangeira da tabela Departamento
+ALTER TABLE Departamento ADD FOREIGN KEY (fkGerente) REFERENCES Funcionario (idFunc);
+
+-- Configurando a chave estrangeira do supervisor na tabela Funcionario
+ALTER TABLE Funcionario ADD FOREIGN KEY (fkSupervisor) REFERENCES Funcionario (idFunc);
+
+-- __________________________________________________________________________________________________
+
+-- 1-) Exibindo os dados de cada tabela separadamente
+SELECT * FROM Departamento;
+SELECT * FROM Funcionario;
+SELECT * FROM Projeto;
+SELECT * FROM FuncProj;
+
+-- 2-) Inserindo funcionário exigido no exercicio
+INSERT INTO Funcionario (idFunc, nomeFunc, salario, sexo, fkSupervisor, dataNasc, fkDepto)
+	VALUES (NULL, 'Cecília Ribeiro', 2800, 'f', NULL, '1980-04-05', 104);
+    
+-- 3-) Inserindo funcionário exigido no exercicio
+INSERT INTO Funcionario (idFunc, nomeFunc, salario, sexo, fkSupervisor, dataNasc, fkDepto)
+	VALUES (3, 'Alice Sousa', 2800, 'f', 4, '1980-04-05', 104);
+    
+-- 4-) Inserindo funcionário exigido no exercicio
+INSERT INTO Funcionario (idFunc, nomeFunc, salario, sexo, fkSupervisor, dataNasc, fkDepto)
+	VALUES (9, 'Cecília Ribeiro', 2800, 'f', 4, '1980-04-05', 107);
+    
+-- 5-) Inserindo funcionário exigido no exercicio
+INSERT INTO Funcionario (idFunc, nomeFunc, salario, sexo, fkSupervisor, dataNasc, fkDepto)
+	VALUES (9, 'Cecília Ribeiro', 2800, 'f', 4, '1980-04-05', 104);
+    
+-- 6-) Excluindo o registro da tabela FuncProj em que o fkProj seja 10 e a fkFunc seja 3
+DELETE FROM FuncProj WHERE fkProj = 10 AND fkFunc = 3;
+
+-- 7-) Excluindo o registro da tabela Funcionario de idFunc = 4
+DELETE FROM Funcionario WHERE idFunc = 4;
+
+-- 8-) Excluindo o registro da tabela Funcionario de idFunc = 2
+DELETE FROM Funcionario WHERE idFunc = 2;
+
+-- 9-) Alterar o salário do funcionário 3 para 2800
+UPDATE Funcionario SET salario = 2800 WHERE idFunc = 3;
+
+-- 10-) Alterar o fkDepto do funcionario 3 para 101
+UPDATE Funcionario SET fkDepto = 101 WHERE idFunc = 3;
+
+-- 11-) Alterar o fkDepto do funcionario 3 para 107
+UPDATE Funcionario SET fkDepto = 107 WHERE idFunc = 3;
+
+-- 12-) Exibir a data de nascimnento e o salario do funcionario
+SELECT nomeFunc, salario FROM Funcionario WHERE nomeFunc = 'Joao Silva';
+
+-- 13-) Exiba uma lista dos salários de todos os funcionários
+SELECT salario FROM Funcionario;
+
+-- 14-) Exiba uma lista dos salários distintos de todos os funcionários
+SELECT DISTINCT salario FROM Funcionario;
+
+-- 15-) Exibir os dados dos funcionários ordenados pelo nome
+SELECT * FROM Funcionario ORDER BY nomeFunc;
+
+-- 16-) Exibir os dados dos funcionários ordenados pelo salário de forma decrescente
+SELECT * FROM Funcionario ORDER BY salario DESC;
